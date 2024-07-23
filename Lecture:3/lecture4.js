fc84b479f77bdfb7164ae9a8106790e1a1a889f0
deepakparwani12345 <105700045+deepakparwani12345@users.noreply.github.com>
2024-07-23 07:46:16 +0000

    Lecture 4 completed

---
+++ Lecture:3/lecture4.js
@@ -0,0 +1,21 @@
+var name = "deepak"
+// var name ="parwani"
+// console.log(name); //parwani
+// name = "deepu"
+// console.log(name);//deepu
+
+let rollno = 12
+//let rollno=13 //SyntaxError: Identifier 'rollno' has already been declared
+rollno=14 //14
+// console.log(rollno);
+
+
+const id =1234
+// const id = 43211 //SyntaxError: Identifier 'id' has already been declared
+// id= 4321 // TypeError: Assignment to constant variable.
+
+// console.log(id);
+
+console.table([name,rollno,id]);
+
+
